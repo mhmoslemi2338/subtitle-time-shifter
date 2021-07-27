cd38832d35b0638cde23d2da9183d0ff8311ac2c
mohammad <mhmoslemi2338@.com>
2021-07-27 13:12:01 +0430

    adding files

---
+++ READme.md
@@ -0,0 +1,12 @@
+﻿**resync your subtilte with movies**
+
+unsync subtitles is a very common issue , with some players you can seek subtilte forwark or backward but the problem is when you want to have two
+subtitles simuoultaniesly and one of them is not sync usually the easiest way is to find another subtitle which consumes time and energy
+but what if you can change the original subtitle file 
+for example if you want to make sub-file 1.3seconds faster you can easy navigate your cmd(on windows) or terminal(on linux) to subtitle directory and
+do this :
+
+**windows :** >python sub_shift.py House.of.Cards.2013.S01E05.srt 1.3
+
+
+**Linux :** $ python3 sub_shift.py House.of.Cards.2013.S01E05.srt 1.3
---
+++ sub_shift.py
@@ -0,0 +1,60 @@
+import re
+import sys
+
+
+
+
+
+
+sec=float(sys.argv[2])
+
+def convert(seconds):
+    min_, sec = divmod(seconds, 60)
+    mili=sec-int(sec)
+    hour, min_ = divmod(min_, 60)
+    return "%02d:%02d:%02d,%03d" % (hour, min_, sec,1000*mili)
+
+
+
+with open(sys.argv[1],'r', encoding="utf8") as f:
+    content=f.readlines()
+
+content = [x.split('\n') for x in content]
+
+
+
+ftr = [3600,60,1,0.001]
+
+for i,row in enumerate(content):
+    try:
+        row_=row[0].split(' ')
+        
+        tmp=re.search("\d\d:\d\d:\d\d",row_[0]).string
+        time1=sum([a*b for a,b in zip(ftr, map(int,re.split(',|:',tmp)))])
+        time1+=sec
+        
+        tmp=re.search("\d\d:\d\d:\d\d",row_[2]).string
+        time2=sum([a*b for a,b in zip(ftr, map(int,re.split(',|:',tmp)))])
+        time2+=sec
+        
+        time1=convert(time1)
+        time2=convert(time2)
+            
+        content[i]=time1+" --> "+time2
+
+
+    except:
+       pass
+        
+    
+
+
+with open(sys.argv[1],'w', encoding="utf8") as f:
+    
+    for row in content:
+        line=''
+        for row2 in row:
+            line+=row2
+
+        f.write(line+'\n')
+
